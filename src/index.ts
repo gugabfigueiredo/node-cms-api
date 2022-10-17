a7609539baf50410caba7582058eb3f383a0a8ed
Gustavo Figueiredo <gustavo.figueiredo@bemobi.comn.br>
2022-10-17 08:31:02 -0300

    server is up and running

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Route, Server } from "./server";
 import { Logger, LOG_LEVEL } from "mini-ts-logger";
 import { ContentHandler } from "./handlers";
-import {Database} from "./database";
+import { Database } from "./database";
 
 const CONTEXT: string = process.env.CMS_CONTEXT ?? "cms-api"
 const LOGGING: string = process.env.CMS_LOG_LEVEL ?? LOG_LEVEL.DEBUG
@@ -9,16 +9,18 @@ const logger = new Logger(console, LOGGING, { context: CONTEXT })
 
 const dbCredentials = {
     url: process.env.CMS_DATABASE_CONNECTION_STRING,
-    user: process.env.CMS_DATABASE_USERNAME,
-    password: process.env.CMS_DATABASE_PASSWORD,
-    host: process.env.CMS_DATABASE_HOST,
-    port: parseInt(process.env.CMS_DATABASE_PORT || ""),
-    database: process.env.CMS_DATABASE_NAME
+    user: process.env.CMS_DATABASE_USERNAME ?? "cms-user",
+    password: process.env.CMS_DATABASE_PASSWORD ?? "123qwe",
+    host: process.env.CMS_DATABASE_HOST ?? "localhost",
+    port: parseInt(process.env.CMS_DATABASE_PORT ?? "5432"),
+    database: process.env.CMS_DATABASE_NAME ?? "cms"
 }
+
 try {
     const db = new Database(dbCredentials, logger)
 
     const handler = new ContentHandler(db, logger);
+    handler.logger.I("handler initiated successfully")
 
     const server = Server(`/${CONTEXT}`, Route((r) => {
         r.use("/content", Route((r) => {
@@ -32,7 +34,7 @@ try {
 
     const PORT: any = process.env.CMS_PORT ?? 8080;
     server.listen(`${PORT}`, () => {
-        logger.I(`Server started at port 3333`);
+        logger.I(`Server started at port ${PORT}`);
     })
 } catch (e) {
     logger.E("failed to start server", e)
