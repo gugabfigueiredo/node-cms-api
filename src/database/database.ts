a48ecd011dca67c0ba0ca96dcc5ce5111b97c098
Gustavo Figueiredo <gustavo.figueiredo@bemobi.comn.br>
2022-10-17 08:24:45 -0300

    fix query issues and promise types

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,6 +1,6 @@
-import {Client, QueryResultRow} from "pg";
+import {Client} from "pg";
 import {Logger} from "mini-ts-logger";
-import {parseQueryParams, QPaginatedResult, QResult, Query} from "./query";
+import {parseQueryParams, QPaginatedResult, QResult} from "./query";
 import format from "pg-format";
 
 export type ModelConstructor<T extends Model> = {
@@ -22,7 +22,7 @@ export class Model {
 }
 
 export type Credentials = {
-    url?: string;
+    connectionString?: string;
     user?: string;
     host?: string;
     port?: number;
@@ -87,22 +87,24 @@ export class Database implements DB {
             )
     }
 
-    async read<T extends Model>(model: T, qParams: Query | any = {}) : Promise<QResult | QPaginatedResult> {
+    read = async <T extends Model>(model: T, qParams: {[k: string]: any} = {}) : Promise<QPaginatedResult> => {
 
         const db = this.client.database
-        const { data = {}, table } = model
-        const { order = [], page = 0, pageSize = 0, search = {} } = qParams
+        const { data, table } = model
+        const { order, page, pageSize, where } = parseQueryParams({...data, ...qParams})
 
         const base = `
-            SELECT * from ${format("%I.%I", db, table())}
-            WHERE ${Object.entries({ ...parseQueryParams(data), ...search }).join(" and ")}
+            SELECT * from ${format("%I.%I.%I", db, ...table())}
+            ${where ? `WHERE ${where}` : ""}
             ${order ? `ODER BY ${order.join(", ")}` : ""}
         `
 
+        this.logger.I("running initial query", { db, table: table(), query: base })
         return this.client.query(base).then(
             result => {
+                const { rowCount, rows } = result
                 if (pageSize) {
-                    const pageCount = Math.ceil(result.rowCount / pageSize)
+                    const pageCount = Math.ceil(rowCount / pageSize)
                     const paginated = `
                         ${base}
                         LIMIT ${pageSize}
@@ -110,36 +112,35 @@ export class Database implements DB {
                     `
                     return this.client.query(paginated).then(
                         result => {
-                            const { oid, rowCount, rows } = result
-                            this.logger.I("query executed successfully", { oid, rowCount })
+                            const { rowCount, rows } = result
+                            this.logger.I("query executed successfully", { rowCount })
                             return { page, pageSize, pageCount, ...result, rows: rows.map(r => model.new(r)) }
                         },
                         reason => {
-                            this.logger.F("failed to execute paginated query", reason, { query: paginated })
+                            this.logger.E("failed to execute paginated query", reason, { query: paginated })
                             return { error: reason, rowCount: 0, rows: [], oid: 0, command: "", fields: [] }
                         }
                     )
                 }
-                const { oid, rowCount, rows } = result
-                this.logger.I("query executed successfully", { oid, rowCount })
+                this.logger.I("query executed successfully", { rowCount, pageSize })
                 return { ...result, rows: rows.map(r => model.new(r)) }
             },
             reason => {
-                this.logger.F("failed to execute query", reason, { query: base })
+                this.logger.E("failed to execute query", reason, { query: base })
                 return { error: reason, rowCount: 0, rows: [], oid: 0, command: "", fields: [] }
             }
         )
     }
 
-    async update<T extends Model>(model: T) : Promise<QResult> {
+    update = async <T extends Model>(model: T) : Promise<QResult> => {
 
         const db = this.client.database
         const { data, table } = model
         const columns = Object.entries(data).filter(([k,]) => k !== "id")
-        const where = parseQueryParams({id: data.id})
+        const { where } = parseQueryParams({id: data.id})
 
         const queryString = `
-            UPDATE ${format("%I.%I", db, table())}
+            UPDATE ${format("%I.%I.%I", db, ...table())}
             SET ${columns.map(([k, v]) => format("%I = %L", k, v)).join(", ")}
             WHERE ${where}
         `
@@ -148,12 +149,12 @@ export class Database implements DB {
         return this.client.query(queryString)
             .then(
                 result => {
-                    const { oid, rowCount, rows } = result
-                    this.logger.I("query executed successfully", { oid, rowCount })
+                    const { rowCount, rows } = result
+                    this.logger.I("query executed successfully", { rowCount })
                     return { ...result, rows: rows.map(r => model.new(r)) }
                 },
                 reason => {
-                    this.logger.F("failed to execute query", reason, { query: queryString })
+                    this.logger.E("failed to execute query", reason, { query: queryString })
                     return { error: reason, rowCount: 0, rows: [], oid: 0, command: "", fields: [] }
                 }
             )
